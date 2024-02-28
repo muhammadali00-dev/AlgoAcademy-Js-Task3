e163b2e8c438039403277d827b05340065b4193c
muhammadali00-dev <ehmedovmehemmedeli0@gmail.com>
2024-02-28 18:24:08 +0400

    Task is done
    Necessary modifications were made and The Task has done

--- todaystask.js
+++ todaystask.js
@@ -1,19 +1,20 @@
 function Time(seconds) {
-    let days = (seconds / (86400));
+    let days = Math.floor(seconds / 86400);
     seconds = seconds % 86400;
-    let hours = (seconds / 3600);
+    let hours = Math.floor(seconds / 3600);
     seconds = seconds % 3600;
-    let minutes = (seconds / 60)
-    seconds = seconds % 60
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
 
+    let timeshower = '';
+    timeshower += days + ' Day, ';
+    timeshower += hours + ' Hours, ';
+    timeshower += minutes + ' Minutes, ';
+    timeshower += seconds + ' Seconds ';
 
-
-
+    return timeshower;
 }
 
-let customersec = Number(prompt('Enter number which you want'))
-alert("General Time is:" + Time(customersec))
+let customersec = Number(prompt('Enter number which you want'));
+alert('Time: ' + Time(customersec));
 
-// 1gun = 86400 saniye
-// 1saat = 3600 saniye
-// 1deqiqe = 60saniye
