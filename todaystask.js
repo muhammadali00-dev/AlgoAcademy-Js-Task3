3ad05fb62b3816ff577d7bd1ce065078150ec6b2
muhammadali00-dev <ehmedovmehemmedeli0@gmail.com>
2024-02-28 17:29:42 +0400

    Some research has been done. The day is settled.

---
+++ todaystask.js
@@ -0,0 +1,14 @@
+function Time(seconds) {
+    let days = (seconds / (86400));
+    seconds = seconds % 86400
+
+
+
+}
+
+let customersec = Number(prompt('Enter number which you want'))
+Time(customersec)
+
+// 1gun = 86400 saniye
+// 1saat = 3600 saniye
+// 1deqiqe = 60saniye
